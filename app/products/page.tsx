0cf2a66a86c8f541e0b60bb9c2a1a9c73d23fff4
Abu shakeer <abushakeer2002@gmail.com>
2026-05-01 11:42:25 +0530

    Fix production API URL issue

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -10,12 +10,12 @@ type Product = {
 };
 
 function getBaseUrl() {
-  if (process.env.NEXT_PUBLIC_BASE_URL) {
-    return process.env.NEXT_PUBLIC_BASE_URL;
-  }
-
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
   }
 
   return "http://localhost:3000";
